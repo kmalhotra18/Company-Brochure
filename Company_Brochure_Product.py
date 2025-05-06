1ad3f42c59936cb86ef9b1dbde234ba7597afe08
kmalhotra18 <malhotra.kunal@gmail.com>
2025-05-05 21:02:25 -0400

    Update Company_Brochure_Product.py
    Updated system prompt

--- Company_Brochure_Product.py
+++ Company_Brochure_Product.py
@@ -105,15 +105,15 @@ def get_all_details(url):
         result += Website(link["url"]).get_contents()
     return result
 
-system_prompt = "You are an assistant that analyzes the contents of several relevant pages from a company website \
-and creates a short brochure about the company for prospective customers, investors and recruits. Respond in markdown.\
-Include details of company culture, customers and careers/jobs if you have the information."
+# system_prompt = "You are an assistant that analyzes the contents of several relevant pages from a company website \
+# and creates a short brochure about the company for prospective customers, investors and recruits. Respond in markdown.\
+# Include details of company culture, customers and careers/jobs if you have the information."
 
 # Or uncomment the lines below for a more humorous brochure - this demonstrates how easy it is to incorporate 'tone':
 
-# system_prompt = "You are an assistant that analyzes the contents of several relevant pages from a company website \
-# and creates a short humorous, entertaining, jokey brochure about the company for prospective customers, investors and recruits. Respond in markdown.\
-# Include details of company culture, customers and careers/jobs if you have the information."
+system_prompt = "You are an assistant that analyzes the contents of several relevant pages from a company website \
+and creates a short humorous, entertaining, jokey brochure about the company for prospective customers, investors and recruits. Respond in markdown.\
+Include details of company culture, customers and careers/jobs if you have the information."
 
 
 def get_brochure_user_prompt(company_name, url):
